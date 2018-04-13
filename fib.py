de3b51fd68b6c60a9fbe948d03f0c376b3c11df6
Max Newport <newpomax@gmail.com>
2018-04-12 18:05:12 -0700

    I think I have it right... hopefully

--- fib.py
+++ fib.py
@@ -25,15 +25,16 @@ Usage
   - './fib.py help': display this help message.
 
 """
-        print help_message
+        print(help_message)
     elif args[0] == "approx" and len(args) == 2:
         phi_approx(int(args[1]))
     elif args[0] == "converge" and len(args) == 1:
         phi_converge()
+    elif args[0] == "converge" and len(args) ==2 :
+        phi_converge(args[1])
     else:
-        print "Error: input not understood.\n" \
-                "    Type './fib.py help' for info on this program."
-
+        print("Error: input not understood.\n" \
+                "    Type './fib.py help' for info on this program.")
 def fib(n):
     """Return nth element of the Fibonacci sequence."""
     # Create the base case
@@ -59,7 +60,7 @@ def phi_approx(n, show_output=True):
     fib_nm1 = fib(n - 1)
     phi = float(fib_n)/fib_nm1
     if show_output:
-        print phi_approx_output_format.format(n, fib_n, fib_nm1, phi)
+        print(phi_approx_output_format.format(n, fib_n, fib_nm1, phi))
     return phi
 
 phi_converge_output_format = \
@@ -67,18 +68,28 @@ phi_converge_output_format = \
     phi_old: {:.25f}
     phi_new: {:.25f}"""
 
-def phi_converge():
+def phi_converge(filename = ""):
     """Keep calculating higher-order Fibonacci approximations to the golden
     ratio until it stops changing (to floating-point precision)."""
-
-    i = 3
-    phi_old = phi_approx(i - 1, show_output=False)
-    phi_new = phi_approx(i)
-    while phi_old != phi_new:
-        i += 1
-        phi_old = phi_new
-        phi_new = phi_approx(i, show_output=False)
-        print phi_converge_output_format.format(i, phi_new, phi_old)
-    print "\nConverged to %.25f" % phi_new
-
+    if(filename!=""):
+        with open(filename, 'w') as f:
+            i = 3
+            phi_old = phi_approx(i - 1, show_output=False)
+            phi_new = phi_approx(i)
+            while phi_old != phi_new:
+                i += 1
+                phi_old = phi_new
+                phi_new = phi_approx(i, show_output=False)
+                f.write(phi_converge_output_format.format(i, phi_new, phi_old))
+            f.write("\nConverged to %.25f" % phi_new)
+    else:       
+        i = 3
+        phi_old = phi_approx(i - 1, show_output=False)
+        phi_new = phi_approx(i)
+        while phi_old != phi_new:
+            i += 1
+            phi_old = phi_new
+            phi_new = phi_approx(i, show_output=False)
+            print(phi_converge_output_format.format(i, phi_new, phi_old))
+        print("\nConverged to %.25f" % phi_new)
 if __name__ == '__main__': main()
